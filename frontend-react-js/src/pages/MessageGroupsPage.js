349bd978f75ad5b77aa93138a01279e13890d686
Aravind V <aravindvcyber@gmail.com>
2023-05-27 07:15:33 +0000

    fix nodes lint

--- frontend-react-js/src/pages/MessageGroupsPage.js
+++ frontend-react-js/src/pages/MessageGroupsPage.js
@@ -4,7 +4,6 @@ import React from "react";
 import DesktopNavigation  from '../components/DesktopNavigation';
 import MessageGroupFeed from '../components/MessageGroupFeed';
 
-import {get} from 'lib/Requests';
 import {checkAuth} from '../lib/CheckAuth';
 import {get} from 'lib/Requests';
 
